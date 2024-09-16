c79a86fbeef800a5efea50ba5f3b41ca639df339
Bhagirath Auti <146720754+bhagirathauti@users.noreply.github.com>
2024-09-16 10:04:16 +0530

    Encapsulation done

--- main.cpp
+++ main.cpp
@@ -23,6 +23,26 @@ public:
         totalCars--;
     }
 
+    // Accessor (getter) for brand
+    string getBrand() const {
+        return brand;
+    }
+
+    // Mutator (setter) for brand
+    void setBrand(const string& newBrand) {
+        brand = newBrand;
+    }
+
+    // Accessor (getter) for color
+    string getColor() const {
+        return color;
+    }
+
+    // Mutator (setter) for color
+    void setColor(const string& newColor) {
+        color = newColor;
+    }
+
     void startCar() {
         if (fuellevel > 0) {
             cout << brand << " " << model << " is Started." << endl;
@@ -30,11 +50,9 @@ public:
             cout << "Out of fuel, please refuel." << endl;
         }
     }
-
     void checkFuel() {
         cout << brand << " " << model << " has " << fuellevel << " litres remaining." << endl;
     }
-
     void refuel(int amount) {
         if (fuellevel + amount <= 100) {
             fuellevel += amount;
@@ -43,6 +61,7 @@ public:
             cout << "Fuel Tank is full." << endl;
         }
     }
+
     void getCarInfo() {
         cout << "Car Information: " << endl;
         cout << "Brand: " << brand << endl;
@@ -50,7 +69,8 @@ public:
         cout << "Color: " << color << endl;
         cout << "Fuel Level: " << fuellevel << "%" << endl;
     }
-     static int getTotalCars() { 
+
+    static int getTotalCars() { 
         return totalCars;
     }
 };
@@ -108,141 +128,8 @@ int main() {
         SUV("Jeep", "Cherokee", "Green", 70)
     };
 
-    int carType, carIndex, action;
-
-    while (true) {
-        cout << "\nChoose Car Type:" << endl;
-        cout << "1. Sedan" << endl;
-        cout << "2. SUV" << endl;
-        cout << "Type: ";
-        cin >> carType;
-
-        if (carType == 1) { 
-            cout << "\nChoose Sedan Car (0 or 1): ";
-            cin >> carIndex;
-
-            if (carIndex < 0 || carIndex >= 2) {
-                cout << "Invalid car index! Try again." << endl;
-                continue;
-            }
-
-            while (true) {
-                cout << "\nChoose an action:" << endl;
-                cout << "1. Start Car" << endl;
-                cout << "2. Drive Car" << endl;
-                cout << "3. Refuel Car" << endl;
-                cout << "4. Check Fuel" << endl;
-                cout << "5. Open Boot" << endl;
-                cout << "6. Get Car Info" << endl;
-                cout << "7. Change Car Type" << endl;
-                cout << "8. Exit" << endl;
-                cout << "Action: ";
-                cin >> action;
-
-                if (action == 7) {
-                    break; 
-                } else if (action == 8) {
-                    cout << "Exiting the game. Goodbye!" << endl;
-                    delete[] sedanCars;
-                    delete[] suvCars;
-                    return 0;
-                }
-
-                switch (action) {
-                    case 1:
-                        sedanCars[carIndex].startCar();
-                        break;
-                    case 2:
-                        sedanCars[carIndex].drive();
-                        break;
-                    case 3: {
-                        int refuelAmount;
-                        cout << "Enter the amount to refuel: ";
-                        cin >> refuelAmount;
-                        sedanCars[carIndex].refuel(refuelAmount);
-                        break;
-                    }
-                    case 4:
-                        sedanCars[carIndex].checkFuel();
-                        break;
-                    case 5:
-                        sedanCars[carIndex].openBoot();
-                        break;
-                    case 6:
-                        sedanCars[carIndex].getCarInfo();
-                        break;
-                    default:
-                        cout << "Invalid action! Please choose a valid option." << endl;
-                        break;
-                }
-            }
-        } else if (carType == 2) { 
-            cout << "\nChoose SUV Car (0 or 1): ";
-            cin >> carIndex;
-
-            if (carIndex < 0 || carIndex >= 2) {
-                cout << "Invalid car index! Try again." << endl;
-                continue;
-            }
-
-            while (true) {
-                cout << "\nChoose an action:" << endl;
-                cout << "1. Start Car" << endl;
-                cout << "2. Drive Car" << endl;
-                cout << "3. Refuel Car" << endl;
-                cout << "4. Check Fuel" << endl;
-                cout << "5. Open Boot" << endl;
-                cout << "6. Engage 4X4" << endl;
-                cout << "7. Get Car Info" << endl;
-                cout << "8. Change Car Type" << endl;
-                cout << "9. Exit" << endl;
-                cout << "Action: ";
-                cin >> action;
-
-                if (action == 8) {
-                    break;
-                } else if (action == 9) {
-                    cout << "Exiting the game. Goodbye!" << endl;
-                    delete[] sedanCars;
-                    delete[] suvCars;
-                    return 0;
-                }
-
-                switch (action) {
-                    case 1:
-                        suvCars[carIndex].startCar();
-                        break;
-                    case 2:
-                        suvCars[carIndex].drive();
-                        break;
-                    case 3: {
-                        int refuelAmount;
-                        cout << "Enter the amount to refuel: ";
-                        cin >> refuelAmount;
-                        suvCars[carIndex].refuel(refuelAmount);
-                        break;
-                    }
-                    case 4:
-                        suvCars[carIndex].checkFuel();
-                        break;
-                    case 5:
-                        suvCars[carIndex].openBoot();
-                        break;
-                    case 6:
-                        suvCars[carIndex].engage4x4();
-                        break;
-                    case 7:
-                        suvCars[carIndex].getCarInfo();
-                        break;
-                    default:
-                        cout << "Invalid action! Please choose a valid option." << endl;
-                        break;
-                }
-            }
-        } else {
-            cout << "Invalid car type! Please try again." << endl;
-        }
-    }
+    sedanCars[0].setBrand("Lexus"); 
+    cout << "Updated Brand Name, Brand: " << sedanCars[0].getBrand() << endl;
 
     delete[] sedanCars;
     delete[] suvCars;
